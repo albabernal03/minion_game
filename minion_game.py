cdf636fae9928213134093951517b887e43fe0df
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-14 10:42:42 +0100

    creo las pariable puntuacion de cada jugador que es este caso son Kevin y Stuart

--- minion_game.py
+++ minion_game.py
@@ -1,5 +1,9 @@
-def minion_game(string):
-    #your code goes here
+def minion_game(palabra):
+    puntuacion_Kevin = 0
+    puntuacion_Stuart = 0
+
+  
 
 if __name__ == '__main__':
-    palabra = input('Elige una palbra:')
+    palabra = input('Elige una palabra:')
+    minion_game(palabra)
